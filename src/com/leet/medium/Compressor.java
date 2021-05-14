ae8e0b206284a8fb292c3003d0728c5683f73faf
himsingh <himrajaccess@gmail.com>
2021-05-14 01:36:32 -0700

    Add to my Git hub account

--- src/com/leet/medium/Compressor.java
+++ src/com/leet/medium/Compressor.java
@@ -2,7 +2,8 @@ package com.leet.medium;
 
 public class Compressor {
     public static void main(String[] args) {
-        System.out.println(compressor("abbbc"));
+        System.out.println(compressor("aabbbc"));
+        System.out.println(deCompress(compressor("aabbbc")));
     }
 
     public static String compressor(String str){
@@ -11,14 +12,32 @@ public class Compressor {
         for(int i=0;i<str.length();i++){
             counter++;
             if(i+1>=str.length()||str.charAt(i)!=str.charAt(i+1)){
-                sb.append(str.charAt(i));
-                if(counter>1) {
+
+                if(counter>0) {
                     sb.append(counter);
                 }
+                sb.append(str.charAt(i));
                 counter=0;
             }
         }
 
         return sb.length() <= str.length() ? sb.toString():str;
+    }
+
+    public static String deCompress(String str){
+        StringBuilder sb = new StringBuilder();
+        int count=0;
+        for(int i=0;i<str.length();i++){
+            if(Character.isDigit(str.charAt(i))){
+                count = str.charAt(i)-'0';
+            }
+            else{
+                while(count>0){
+                    sb.append(str.charAt(i));
+                    count--;
+                }
+            }
+        }
+        return sb.toString();
     }
 }
